0433511c9cb34ef29c60c348afbc2a7ca0dd737d
Ankit Kumar <kumarankit802215@gmail.com>
2025-01-04 16:20:43 +0530

    Sorting : Selection Sort

--- Array/Sorting/SelectionSort.java
+++ Array/Sorting/SelectionSort.java
@@ -1,6 +1,6 @@
 public class SelectionSort{
     public static void main(String args[]){
-        int arr[] = {5, 6, 2, 1, 9, 3};
+        int arr[] = {4, 3, 5, 1, 2, 6, 9, 8};
         SelectionSort(arr);
         PrintSort(arr);
     }
@@ -26,4 +26,23 @@ public class SelectionSort{
             System.err.print( arr[i] +" ");
         }
     }
+
+    // static void selection(int arr[]){
+        
+    //     for(int i=0; i<arr.length; i++){
+            
+            
+    //         int min_idx = i;
+    //         for(int j=i; j<arr.length; j++){
+    //             if(arr[j]<arr[min_idx]){
+    //                 min_idx = j;
+    //             }
+    //         }
+    //         int temp = arr[min_idx];
+    //         arr[min_idx] = arr[i];
+    //         arr[i] = temp;
+    //     }
+
+    //     System.out.print(Arrays.toString(arr));
+    // }
 }
