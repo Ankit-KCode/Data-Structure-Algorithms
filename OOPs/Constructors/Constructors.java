1e1f3f1cb301154946561f54afbd37f5f2945fa4
Ankit Kumar <kumarankit802215@gmail.com>
2025-01-20 14:54:59 +0530

    OOPs : Types of Constructors

--- OOPs/Constructors/Constructors.java
+++ OOPs/Constructors/Constructors.java
@@ -1,8 +1,12 @@
 public class Constructors{
     public static void main (String [] args){
         student std = new student("Alex", 201);
+        student std1 = new student("Max");
+        student std2 = new student(123);
+        student std3 = new student();
         
-        System.out.println(std.name +" " + std.Rollno);
+        System.out.println(std1.name +" " + std2.Rollno);
+        System.out.println(std.name + " " + std.Rollno);
     }
 }
 
@@ -10,14 +14,22 @@ class student{
     String name;
     int Rollno;
 
-    student(String n, int R){ // Constructor
-        name = n;
-        Rollno = R;
+    student(String name){  // Parameterise Constructor
+        this.name = name;
     }
 
-    // student(){ //Constructor
-    //     System.out.println("This is Constructor call.");
-    // }
+    student(int Rollno){  // Parameterise Constructor
+        this.Rollno = Rollno;
+    }
+
+    student(String name, int Rollno){ // Parameterise Constructor
+        this.name = name;
+        this.Rollno = Rollno;
+    }
+
+    student(){ // Non Parameterise Constructor
+        System.out.println("This is Constructor call.");
+    }
 
 
 }
