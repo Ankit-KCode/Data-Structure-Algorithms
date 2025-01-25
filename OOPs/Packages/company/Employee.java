07bed7835733ed56126d843ffba192addfa69361
Ankit Kumar <kumarankit802215@gmail.com>
2025-01-25 14:57:17 +0530

    OOPs : Creating Package

---
+++ OOPs/Packages/company/Employee.java
@@ -0,0 +1,7 @@
+package company;
+// First create a folder(name = packagename). then add some public class files to that package. [then use these classes in main class inside/outside the package]
+public class Employee{
+    public void Display(){
+        System.out.println("This Is Employee.");
+    }
+}
---
+++ OOPs/Packages/main.java
@@ -0,0 +1,8 @@
+import company.Employee;
+// Importing created package company's Employee class.
+public class main{
+    public static void main(String args[]){
+        Employee emp = new Employee();
+        emp.Display();
+    }
+}
