f2e591def4b77d909d7e96569c4ad9acf34ece24
Ankit Kumar <kumarankit802215@gmail.com>
2025-01-29 20:36:39 +0530

    Problems : isPrime (Optimized)

--- Array/Optimized Solutions/isPrime.java
+++ Array/Optimized Solutions/isPrime.java
@@ -1,25 +1,16 @@
 public class isPrime{
     public static void main(String args[]){
-        System.out.print("Prime: " + isPrime(31));
+        System.out.print("Prime: " + isPrime(2));
     }
 
     static boolean isPrime(int n){
         //Corner Case
-        if(n == 2){
-            return true;
-        }
-
-        // for(int i = 2; i<=n-1; i++){
-        //     if(n%i == 0){
-        //         return false;
-        //     }
-        // }
+        if(n == 2) return true;
+        if(n == 1) return false;
 
         //Optimize Method
         for(int i=2; i<=Math.sqrt(n); i++){
-            if(n%i == 0){
-                return false;
-            }
+            if(n%i == 0) return false;
         }
 
         return true;
