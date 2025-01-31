5bb2dbc94af5a4d2462e9adb34ad7f4e635211c1
Ankit Kumar <kumarankit802215@gmail.com>
2025-01-31 16:55:24 +0530

    Recursion : Print 1 to n

--- Recursion/Print1toN.java
+++ Recursion/Print1toN.java
@@ -1,15 +1,14 @@
-public class PrintN{
+public class Print1toN{
     public static void main(String args[]){
         int n = 100; // Prints 1 - n;
-        print(n);
+        int count=0;
+        print(n,count);
     }
 
-    static int count=0;
-    
-    static void print(int n){
+    static void print(int n, int count){
         if(count == n) return;
         count++;
         System.out.print(count+" ");
-        print(n);
+        print(n, count);
     }
 }
