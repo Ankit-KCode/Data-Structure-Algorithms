6d06a6feddbed51f6f1c3c8485e79896dbb91164
Ankit Kumar <kumarankit802215@gmail.com>
2025-02-10 16:46:10 +0530

    Sorting : Insertion Sort

--- Array/Sorting/InsertionSort.java
+++ Array/Sorting/InsertionSort.java
@@ -1,27 +1,22 @@
+import java.util.*;
 public class InsertionSort{
     public static void main(String args[]){
-        int arr[] = {7, 9, 8, 10, 5, 3, 6, 2, 1, 4};
+        int arr[] = {7, 9, 8, 10, 10, 5, 3, 6, 2, 1, 4};
         InsertionSort(arr);
-        PrintSort(arr);
     }
 
     static void InsertionSort(int arr[]){
-        for(int i=1; i<arr.length; i++){
-            int curr = arr[i];
-            int prev = i-1;
-
-            while(prev >= 0 && arr[prev] > curr){
-                arr[prev+1] = arr[prev];
-                prev--;
-            }
-
-            arr[prev + 1] = curr;
-        }
-    }
-
-    static void PrintSort(int arr[]){
         for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
+
+            int j = i;
+            while(j > 0 && arr[j-1] > arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j--;
+            }
         }
+        System.out.println(Arrays.toString(arr));
     }
+
 }
