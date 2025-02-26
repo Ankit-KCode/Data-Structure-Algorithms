93605da47624b658c0adf395ab02cf85cd1b627f
Ankit Kumar <kumarankit802215@gmail.com>
2025-02-26 19:20:56 +0530

    ArrayList : Basics 2.0

--- ArrayList/List_Basics.java
+++ ArrayList/List_Basics.java
@@ -3,6 +3,7 @@ public class List_Basics{
     public static void main(String args[]){
         ArrayList<Integer> list = new ArrayList<>();
 
+        // Append Elements into ArrayList
         list.add(2);
         list.add(4);
         list.add(5);
@@ -10,7 +11,26 @@ public class List_Basics{
 
         System.out.println(list);
 
-        list.remove(0);
+        // Remove any Elements
+        list.remove(0); 
         System.out.println(list);
+
+        // change specific element
+        list.set(1, 10); // Overlap the element
+        System.out.println(list);
+
+        // checking the element in list
+        System.out.println(list.contains(1));
+        System.out.println(list.contains(99));
+
+        // Adding element at specific index
+        list.add(0, 100); // O(n)
+        System.out.println(list);
+
+        // Access/Retrieve the specific index element
+        System.out.println(list.get(0));
+
+        // To get the Size of the ArrayList
+        System.out.println(list.size());
     }
 }
