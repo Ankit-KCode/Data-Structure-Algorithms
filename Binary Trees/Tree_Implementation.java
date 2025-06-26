a568065678e3ef34a89d4367660935911101871e
Ankit Kumar <kumarankit802215@gmail.com>
2025-06-26 21:20:52 +0530

    Binary Trees : Tree_Implementation

---
+++ Binary Trees/Tree_Implementation.java
@@ -0,0 +1,21 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int key){
+        data = key;
+    }
+}
+
+public class Tree_Implementation{
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.right.right = new Node(5);
+    }
+}
+
+// add Leetcode Traversal method
