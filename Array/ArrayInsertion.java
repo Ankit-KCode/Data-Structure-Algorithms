dbe2778277dd9ce6a73cde593ed669bd5b8a8635
Ankit Kumar <kumarankit802215@gmail.com>
2025-07-24 15:16:16 +0530

    Arrays : Updates

--- Array/ArrayInsertion.java
+++ Array/ArrayInsertion.java
@@ -21,7 +21,7 @@ public class ArrayInsertion{
         }
         arr[position] = element;
 
-        for(int i=0; i<arr.length-1; i++){
+        for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
     }
