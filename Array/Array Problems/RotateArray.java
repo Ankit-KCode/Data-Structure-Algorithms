8a555ea80830c3509a9cafc041a6d9e3cbd453f0
Ankit Kumar <kumarankit802215@gmail.com>
2025-10-27 16:24:08 +0530

    Array : Update

--- Array/Array Problems/RotateArray.java
+++ Array/Array Problems/RotateArray.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 
 public class RotateArray{
     public static void main(String args[]){
-        int nums[] = {-1,-100,3,99};
+        int nums[] = {-1,-100, 3, 99};
         int n = nums.length;
         int k = 2;
         if(k>n) k=k%n;
 
-        reverse(0, n-1, nums);
-        reverse(0, k-1, nums);
-        reverse(k, n-1, nums);
+        reverse(0, n-1, nums); // {99, 3, -100, -1}
+        reverse(0, k-1, nums); // {3, 99, -100, -1}
+        reverse(k, n-1, nums); // {3, 99, -1, -100}
 
         System.out.println(Arrays.toString(nums));
     }
